b8b54e175dea6ee25e0e11bac2916261ffe97bd9
Alexandr Vanchukov <61061837+AlexandrVanchukov@users.noreply.github.com>
2024-08-23 16:02:52 +0800

    Добавил компонент ошибки

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import App from './components/app';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import { fetchOffersAction } from './store/api-actions';
+import ErrorMessage from './components/error-message/error-message';
 
 store.dispatch(fetchOffersAction());
 
@@ -16,6 +17,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <ErrorMessage />
       <App
         reviews = {reviews}
       />
